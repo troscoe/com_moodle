a84d3e0616336f01a43bb2d8cc057b13a80eac52
Tristan Roscoe <troscoe@gmail.com>
2016-03-02 16:27:30 -0500

    Add a model for default view
    Added models folder and model for default view

---
+++ site/models/helloworld.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_moodle
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * Moodle Model
+ *
+ * @since  0.0.1
+ */
+class MoodleModelMoodle extends JModelItem
+{
+	/**
+	 * @var string message
+	 */
+	protected $message;
+ 
+	/**
+	 * Get the message
+         *
+	 * @return  string  The message to be displayed to the user
+	 */
+	public function getMsg()
+	{
+		if (!isset($this->message))
+		{
+			$this->message = 'Moodle';
+		}
+ 
+		return $this->message;
+	}
+}
--- site/views/moodle/view.html.php
+++ site/views/moodle/view.html.php
@@ -27,7 +27,15 @@ class MoodleViewMoodle extends JViewLegacy
 	function display($tpl = null)
 	{
 		// Assign data to the view
-		$this->msg = 'Moodle';
+		$this->msg = $this->get('Msg');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JLog::add(implode('<br />', $errors), JLog::WARNING, 'jerror');
+ 
+			return false;
+		}
  
 		// Display the view
 		parent::display($tpl);
