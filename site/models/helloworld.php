f4ecc8df43322a7d955261c649c30def1aba2785
Tristan Roscoe <troscoe@gmail.com>
2016-03-02 16:40:29 -0500

    Add a variable request in the menu type
    Modifications to the model to read in a variable request for the menu

--- site/models/helloworld.php
+++ site/models/helloworld.php
@@ -31,7 +31,19 @@ class MoodleModelMoodle extends JModelItem
 	{
 		if (!isset($this->message))
 		{
-			$this->message = 'Moodle';
+			$jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+ 
+			switch ($id)
+			{
+				case 2:
+					$this->message = 'Good bye World!';
+					break;
+				default:
+				case 1:
+					$this->message = 'Hello World!';
+					break;
+			}
 		}
  
 		return $this->message;
