2ca3041b373a6764734d30d5801b81f37d6d945d
Tristan Roscoe <troscoe@gmail.com>
2016-03-02 16:04:18 -0500

    Add default display view
    Add a view to a basic component.

--- site/controller.php
+++ site/controller.php
@@ -6,16 +6,13 @@
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
- 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
- 
-// Get an instance of the controller prefixed by Moodle
-$controller = JControllerLegacy::getInstance('Moodle');
- 
-// Perform the Request task
-$input = JFactory::getApplication()->input;
-$controller->execute($input->getCmd('task'));
- 
-// Redirect if set by the controller
-$controller->redirect();
+/**
+ * Moodle Component Controller
+ *
+ * @since  0.0.1
+ */
+class MoodleController extends JControllerLegacy
+{
+}
