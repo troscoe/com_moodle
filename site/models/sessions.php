4ca675a22a8e6949d383e2c3d644808c79776d9b
Tristan Roscoe <troscoe@gmail.com>
2016-03-07 01:07:07 -0500

    Map facetoface name and session data to columns

--- site/models/sessions.php
+++ site/models/sessions.php
@@ -18,6 +18,103 @@ defined('_JEXEC') or die('Restricted access');
 class MoodleModelSessions extends JModelList
 {
 	/**
+	 * Gets the list of users and adds expensive joins to the result set.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 *
+	 * @since   1.6
+	 */
+	public function getItems()
+	{
+		// Get a storage key.
+		$store = $this->getStoreId();
+
+		// Try to load the data from internal storage.
+		if (empty($this->cache[$store]))
+		{
+			$items = parent::getItems();
+
+			// Bail out on an error or empty list.
+			if (empty($items))
+			{
+				$this->cache[$store] = $items;
+				return $items;
+			}
+
+			// First pass: get list of the session id's and reset the counts.
+			$sessionIds = array();
+
+			foreach ($items as $item)
+			{
+				$sessionIds[] = (int) $item->id;
+				$item->field_count = 0;
+				$item->fields = array();
+				$item->date_count = 0;
+				$item->dates = array();
+			}
+
+			// Get the counts from the database only for the users in the list.
+			$db    = $this->getDbo();
+			$query = $db->getQuery(true);
+			// Join over the group mapping table.
+			$query->select('data.sessionid, COUNT(data.fieldid) AS field_count')
+				->from('#__moodle_facetoface_session_data AS data')
+				->where('data.sessionid IN (' . implode(',', $sessionIds) . ')')
+				->group('data.sessionid')
+				// Join over the user groups table.
+				->join('LEFT', '#__moodle_facetoface_session_field AS field ON field.id = data.fieldid')
+				->where('field.showinsummary = 1');
+			$db->setQuery($query);
+			// Load the counts into an array indexed on the user id field.
+			try
+			{
+				$sessionFields = $db->loadObjectList('sessionid');
+			}
+			catch (RuntimeException $e)
+			{
+				$this->setError($e->getMessage());
+				return false;
+			}
+			$query->clear()
+				->select('dates.sessionid, COUNT(dates.id) AS date_count')
+				->from('#__moodle_facetoface_sessions_dates AS dates')
+				->where('dates.sessionid IN (' . implode(',', $sessionIds) . ')')
+				->group('dates.sessionid');
+			$db->setQuery($query);
+			// Load the counts into an array indexed on the aro.value field (the user id).
+			try
+			{
+				$sessionDates = $db->loadObjectList('sessionid');
+			}
+			catch (RuntimeException $e)
+			{
+				$this->setError($e->getMessage());
+				return false;
+			}
+			// Second pass: collect the group counts into the master items array.
+			foreach ($items as &$item)
+			{
+				if (isset($sessionFields[$item->id]))
+				{
+					$item->field_count = $sessionFields[$item->id]->field_count;
+					// Group_concat in other databases is not supported
+					$item->fields = $this->_getSessionFields($item->id);
+				}
+				if (isset($sessionDates[$item->id]))
+				{
+					$item->date_count = $sessionDates[$item->id]->date_count;
+					// Group_concat in other databases is not supported
+					$item->dates = $this->_getSessionDates($item->id);
+				}
+			}
+			// Add the items to the internal cache.
+			$this->cache[$store] = $items;
+		}
+
+		return $this->cache[$store];
+	}
+
+	/**
 	 * Method to build an SQL query to load the list data.
 	 *
 	 * @return      string  An SQL query
@@ -29,9 +126,67 @@ class MoodleModelSessions extends JModelList
 		$query = $db->getQuery(true);
  
 		// Create the base select statement.
-		$query->select('*')
-                ->from($db->quoteName('#__moodle_facetoface_sessions'));
+		$query->select('session.id, f2f.name, session.capacity')
+                ->from('#__moodle_facetoface_sessions AS session')
+                ->join('LEFT', '#__moodle_facetoface AS f2f ON f2f.id = session.facetoface');
  
 		return $query;
+	}
+
+	/**
+	 * SQL server change
+	 *
+	 * @param   integer  $sessionid  Session identifier
+	 *
+	 * @return  string   Groups titles imploded :$
+	 */
+	protected function _getSessionFields($sessionid)
+	{
+		// Load the profile data from the database.
+		$db = JFactory::getDbo();
+		$db->setQuery(
+			'SELECT field.name, field.shortname, data.data FROM #__moodle_facetoface_session_data AS data' .
+				' LEFT JOIN #__moodle_facetoface_session_field AS field ON field.id = data.fieldid' .
+				' WHERE data.sessionid = ' . (int) $sessionid . ' AND field.showinsummary = 1' .
+				' ORDER BY data.fieldid'
+		);
+		try
+		{
+			$results = $db->loadAssocList();
+		}
+		catch (RuntimeException $e)
+		{
+			$result = array();
+		}
+
+		return $results;
+	}
+
+	/**
+	 * SQL server change
+	 *
+	 * @param   integer  $sessionid  Session identifier
+	 *
+	 * @return  string   Groups titles imploded :$
+	 */
+	protected function _getSessionDates($sessionid)
+	{
+		// Load the profile data from the database.
+		$db = JFactory::getDbo();
+		$db->setQuery(
+			'SELECT from_unixtime(timestart) AS timestart, from_unixtime(timefinish) AS timefinish FROM #__moodle_facetoface_sessions_dates' .
+				' WHERE sessionid = ' . (int) $sessionid .
+				' ORDER BY sessionid'
+		);
+		try
+		{
+			$results = $db->loadAssocList();
+		}
+		catch (RuntimeException $e)
+		{
+			$result = array();
+		}
+
+		return $results;
 	}
 }
--- site/views/sessions/tmpl/default.php
+++ site/views/sessions/tmpl/default.php
@@ -18,6 +18,138 @@ defined('_JEXEC') or die('Restricted access');
 					<?php echo $this->escape($this->params->get('page_heading')); ?>
 				</h1>
 			<?php endif; ?>
+
+			<?php if (empty($this->items)) : ?>
+				<p><?php echo "No sessions"; ?></p>
+			<?php else : ?>
+				<form action="<?php echo htmlspecialchars(JUri::getInstance()->toString()); ?>" method="post" name="adminForm" id="adminForm" class="form-inline">
+	
+					<?php if ($this->params->get('show_headings') || $this->params->get('filter_field') != 'hide' || $this->params->get('show_pagination_limit')) :?>
+					<fieldset class="filters btn-toolbar clearfix">
+						<?php if ($this->params->get('filter_field') != 'hide') :?>
+							<div class="btn-group">
+								<label class="filter-search-lbl element-invisible" for="filter-search">
+									<?php echo JText::_('COM_CONTENT_' . $this->params->get('filter_field') . '_FILTER_LABEL') . '&#160;'; ?>
+								</label>
+								<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('list.filter')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_CONTENT_FILTER_SEARCH_DESC'); ?>" placeholder="<?php echo JText::_('COM_CONTENT_' . $this->params->get('filter_field') . '_FILTER_LABEL'); ?>" />
+							</div>
+						<?php endif; ?>
+						<?php if ($this->params->get('show_pagination_limit')) : ?>
+							<div class="btn-group pull-right">
+								<label for="limit" class="element-invisible">
+									<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
+								</label>
+								<?php echo $this->pagination->getLimitBox(); ?>
+							</div>
+						<?php endif; ?>
+
+						<input type="hidden" name="filter_order" value="" />
+						<input type="hidden" name="filter_order_Dir" value="" />
+						<input type="hidden" name="limitstart" value="" />
+						<input type="hidden" name="task" value="" />
+					</fieldset>
+					<?php endif; ?>
+
+
+					<table class="sessions table table-striped table-bordered table-hover">
+						<?php
+						$headerCourse    			= '';
+						$headerDate    			= '';
+						$headerLength	     	= '';
+						$headerStatus   		= '';
+						?>
+						<!--<?php //if ($this->params->get('show_headings')) : ?>-->
+							<?php
+							$headerCourse   = 'headers="sessionslist_header_course"';
+							$headerDate   = 'headers="sessionslist_header_date"';
+							$headerLength   = 'headers="sessionslist_header_length"';
+							$headerStatus   = 'headers="sessionslist_header_status"';
+							?>
+						<thead>
+							<tr>
+								<th id="sessionslist_header_course">
+									<?php echo JHtml::_('grid.sort', 'COM_MOODLE_SESSION_COURSE', 'a.course', $listDirn, $listOrder); ?>
+								</th>
+								<?php foreach ($this->items[0]->fields as $i => $field) : ?>
+									<th id="sessionslist_header_<?php echo $field['shortname']; ?>">
+										<?php echo JHtml::_('grid.sort', 'COM_MOODLE_SESSION_'.strtoupper($field['shortname']), 'a.'.$field['shortname'], $listDirn, $listOrder); ?>
+									</th>
+								<?php endforeach; ?>
+								<th id="sessionslist_header_date">
+									<?php echo JHtml::_('grid.sort', 'COM_MOODLE_SESSION_DATE', 'a.date', $listDirn, $listOrder); ?>
+								</th>
+								<th id="sessionslist_header_length">
+									<?php echo JHtml::_('grid.sort', 'COM_MOODLE_SESSION_LENGTH', 'a.length', $listDirn, $listOrder); ?>
+								</th>
+								<th id="sessionslist_header_status">
+									<?php echo JHtml::_('grid.sort', 'COM_MOODLE_SESSION_STATUS', 'a.status', $listDirn, $listOrder); ?>
+								</th>
+							</tr>
+						</thead>
+						<!--<?php //endif; ?>-->
+						<tbody>
+
+							<?php foreach ($this->items as $i => $session) : ?>
+								<tr class="sessions-list-row<?php echo $i % 2; ?>" >
+									<td <?php echo $headerCourse; ?> class="list-course">
+										<a href="#">
+											<?php echo $this->escape($session->name); ?>
+										</a>
+									</td>
+									<?php foreach ($session->fields as $i => $field) : ?>
+									<td <?php echo 'headers="sessionslist_header_'.$field['shortname'] ?> class="list-<?php echo $field['shortname']; ?>">
+										<?php echo $this->escape($field['data']); ?>
+									</td>
+									<?php endforeach; ?>
+									<td <?php echo $headerDate; ?> class="list-date">
+										<?php
+										echo JHtml::_(
+											'date', $session->dates['0']['timestart'],
+											$this->escape($this->params->get('date_format', JText::_('DATE_FORMAT_LC3')))
+										); ?>
+									</td>
+
+									<td <?php echo $headerLength; ?> class="list-length">
+										<?php echo $session->date_count . ' day(s)'; ?>
+									</td>
+									<td <?php echo $headerStatus; ?> class="list-status">
+										<?php echo $session->field_count ?>
+									</td>
+
+
+
+
+
+
+
+
+
+								</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+				<?php endif; ?>
+
+				<?php // Add pagination links ?>
+				<?php if (!empty($this->items)) : ?>
+					<?php if (($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)) : ?>
+					<div class="pagination">
+
+						<?php if ($this->params->def('show_pagination_results', 1)) : ?>
+							<p class="counter pull-right">
+								<?php echo $this->pagination->getPagesCounter(); ?>
+							</p>
+						<?php endif; ?>
+
+						<?php echo $this->pagination->getPagesLinks(); ?>
+					</div>
+					<?php endif; ?>
+
+
+
+
+				</form>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
